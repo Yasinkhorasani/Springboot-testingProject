751942933d42c91eb414d72f7c364b56345965cc
Yasin khorasani <yasinkhorasani@yahoo.com>
2023-01-18 20:11:35 +0100

    integration test for create employee REST API

--- src/test/java/net/javaguides/springboot/integration/EmployeeControllerITest.java
+++ src/test/java/net/javaguides/springboot/integration/EmployeeControllerITest.java
@@ -1,12 +1,24 @@
 package net.javaguides.springboot.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -27,4 +39,31 @@ public class EmployeeControllerITest {
     void setup(){
         employeeRepository.deleteAll();
     }
+
+    @Test
+    public void givenEmployeeObject_whenCreateEmployee_thenReturnSaveEmployee() throws Exception{
+
+        //given setup
+        Employee employee = Employee.builder()
+                .firstName("Yasin")
+                .lastName("Khorasani")
+                .email("yasinkhorsani@yahoo.com")
+                .build();
+
+        //when - action or behavior that we are going to test
+        ResultActions response = mockMvc.perform(post("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+
+        //then verify the result or output using assert statement
+        response.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName",
+                        is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName",
+                        is(employee.getLastName())))
+                .andExpect(jsonPath("$.email",
+                        is(employee.getEmail())));
+    }
+
 }
