66ce62f68006a008b34a205cd74c905c2ba86552
Yasin khorasani <yasinkhorasani@yahoo.com>
2023-01-19 10:11:53 +0100

    Using @DynaicPropertySource

--- src/test/java/net/javaguides/springboot/integration/EmployeeControllerIT.java
+++ src/test/java/net/javaguides/springboot/integration/EmployeeControllerIT.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.containers.MySQLContainer;
@@ -34,6 +36,13 @@ public class EmployeeControllerIT {
             .withUsername("username")
             .withPassword("password")
             .withDatabaseName("ems");
+    @DynamicPropertySource
+    public static void dynamicPropertySource(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url",mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+
+    }
 
     // injecting Mockmvc class to make HTTP request using perform() method
     @Autowired
